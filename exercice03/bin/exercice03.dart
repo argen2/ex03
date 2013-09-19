8236a5c138d0bccfc5f28f0da80adf2f698dec8b
argen2 <chiefmulot@gmail.com>
2013-09-19 16:38:28 -0400

    Push du code Dart

--- exercice03/bin/exercice03.dart
+++ exercice03/bin/exercice03.dart
@@ -1,3 +1,17 @@
-void main() {
-  print("Hello, World!");
-}
+// Fizzbuzz
+// Imprimer des chiffres de 1 à 100 
+// Ecrire Fizz pour les multiplicateur de 3
+// Ecrire Buzz pour les multiplicateur de 5 
+// Ecrire Fizzbuzz pour les 2 combinés
+
+main() { 
+    for (int i = 0; i < 100; i++) {
+        if ((i % 15) == 0) {
+            print('FizzBuzz');
+        } else if ((i % 5) == 0) {
+            print('Buzz');
+        } else if ((i % 3) == 0) {
+            print('Fizz');
+        } else {
+            print(i);
+        }}}
